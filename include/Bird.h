158c86f892feb3dd7e974276a5491e411434c184
Soumik0906 <soumikseth8@gmail.com>
2025-03-08 16:22:47 +0530

    Add Bird class with basic features

--- include/Bird.h
+++ include/Bird.h
@@ -3,27 +3,33 @@
 
 #include <SFML/Graphics.hpp>
 
-class Bird {
+class Bird 
+{
 public:
     Bird(sf::RenderWindow &window);
     void jump();
-    void update();
-    void draw(sf::RenderWindow &window);
+    void update(float deltaTime);
+    void draw();
     sf::FloatRect getBounds();
     void reset();
+    void handleInput(const std::optional<sf::Event>& event);
 
 private:
+    sf::RenderWindow& window;
     sf::Texture texture;
     sf::Sprite sprite;
     float gravity{};
     float velocity{};
-    float rotation{};
+    sf::Angle rotation{};
     bool isJumping{};
     bool isFalling{};
     bool alive{};
 
-    static constexpr float GRAVITY{ 9.8f };
-    static constexpr float JUMP_VELOCITY{ -5.0f };
+    static constexpr float GRAVITY{ 600.0f };
+    static constexpr float JUMP_VELOCITY{ -350.0f };
+    static constexpr sf::Angle ROTATION_SPEED{ sf::degrees(200.0f) };
+
+    void updateRotation(float deltaTime);
 };
 
 #endif // BIRD_H
--- src/Bird.cpp
+++ src/Bird.cpp
@@ -1,20 +1,110 @@
 #include "Bird.h"
 
 Bird::Bird(sf::RenderWindow& window) 
-: texture(ROOT_DIR "assets/bird.png")
+: window(window)
+, texture(ROOT_DIR "assets/bird.png")
 , sprite(texture)
 , gravity(GRAVITY)
 , velocity(0)
-, rotation(0)
+, rotation(sf::degrees(0))
 , isJumping(false)
 , isFalling(false)
 , alive(true)
 {
+    texture.setSmooth(true);
     sprite.setScale({ 0.1f, 0.1f });
-    sprite.setPosition({ 100, 300 });
     sprite.setOrigin(sprite.getGlobalBounds().getCenter());
+    sprite.setPosition({ 200, 600 });
 }
 
-void Bird::draw(sf::RenderWindow& window) {
-    window.draw(sprite);
+void Bird::jump() 
+{
+    if (alive) 
+    {
+        velocity = JUMP_VELOCITY;
+        isJumping = true;
+        isFalling = false;
+    }
+}
+
+void Bird::updateRotation(const float deltaTime) 
+{
+    using namespace sf::Literals;
+    if (velocity < 0) 
+    {
+        rotation -= ROTATION_SPEED * deltaTime;
+        if (rotation < -30_deg) 
+        {
+            rotation = -30_deg;
+        }
+    }
+    else if (velocity > 0) 
+    {
+        rotation += ROTATION_SPEED * deltaTime;
+        if (rotation > 60_deg) 
+        {
+            rotation = 60_deg;
+        }
+    }
+
+    sprite.setRotation(rotation);
+}
+
+void Bird::update(const float deltaTime) 
+{
+    if (alive) 
+    {
+        velocity += gravity * deltaTime;
+        sprite.move({ 0, velocity * deltaTime });
+
+        if (velocity > 0) 
+        {
+            isJumping = false;
+            isFalling = true;
+        }
+        else if (velocity < 0) 
+        {
+            isJumping = true;
+            isFalling = false;
+        }
+        updateRotation(deltaTime);
+    }
+}
+
+void Bird::draw() 
+{ 
+    if (alive) 
+    {
+        window.draw(sprite);
+    }
+}
+
+sf::FloatRect Bird::getBounds() 
+{ 
+    return sprite.getGlobalBounds(); 
+}
+
+void Bird::reset() 
+{
+    velocity = 0;
+    rotation = sf::degrees(0);
+    isJumping = false;
+    isFalling = false;
+    alive = true;
+    sprite.setPosition({ 100, 200 });
+    sprite.setRotation(rotation);
+}
+
+void Bird::handleInput(const std::optional<sf::Event>& event) 
+{
+    if (event.has_value()) 
+    {
+        if (event->is<sf::Event::KeyPressed>()) 
+        {
+            if (event->getIf<sf::Event::KeyPressed>()->scancode == sf::Keyboard::Scan::Space) 
+            {
+                jump();
+            }
+        } 
+    }
 }
